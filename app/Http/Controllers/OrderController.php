9611312ac24a5ad5169df3dfc5793478c0bda7f9
Sergey Ukhanov <ukhanov.sergey@gmail.com>
2020-09-20 20:20:52 +0600

    Small response changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,14 +8,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 
+
 class OrderController extends Controller
 {
     public function create(Request $request)
     {
         $this->validate($request, Order::rules());
 
-        $data = $request->all();
+        try {
+            $order = Order::createOrder($request->all());
+        } catch (\Exception $exception) {
+            return response($exception->getMessage(), 500);
+        }
 
-        return response(Order::createOrder($data), 201);
+        return response($order, 201);
     }
 }
